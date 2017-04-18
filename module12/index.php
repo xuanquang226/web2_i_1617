cbdb4861b828132f895c63a3e4bfee948628215f
thanhcong197 <nguyenthanhcongtt3@gmail.com>
2017-04-18 15:37:39 +0700

    upload
    upload

---
+++ module12/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+	<script src="bootstrap-3.3.6-dist/js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+         
+        <link href="css/1.css" rel="stylesheet" type="text/css" />
+	<title>
+		
+	</title>
+</head>
+<body>
+
+	
+	
+	<header class="module-12">
+	<div class="fy-centerer">
+		<h1 class="entry-title" itemprop="name">
+			Pilsner Urquell		</h1>
+			</div>
+
+</header>
+
+
+</div>
+</div>
+
+
+</div>
+	</div>
+
+</body>
+
+</html>
---
+++ module5/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="bootstrap-3.3.6-dist/js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+       <link href="css/1.css" rel="stylesheet" type="text/css" />
+       
+	
+</head>
+<body>
+<div class="module-5">
+<div class="container">
+<div class="col-md-4"> 
+<div class="fy-widget-column fy-flex-column-auto fy-flex-column-tablet-50 fy-flex-column-phone-100"><div id="widget_call_to_action-2" class="fy-widget widget_call_to_action"><div class="fy-widget-container fy-widget-has-button fy-widget-has-link"><h3 class="fy-widget-title"><a href="/menu">Healthy Homemade Cuisine</a></h3><div class="fy-lazy-parent fy-lazy-parent-loaded"><a href="/menu" class="fy-widget-image fy-lazy fy-lazy-loaded" style="opacity: 1; display: block; background-image: url(&quot;https://linguini.forqy.website/wp-content/uploads/2016/08/linguini-cuisine-1.jpg&quot;);"></a><div class="fy-loading">
+	<div class="fy-loader">
+<a href="#"><img src="images/hinh.jpg" width="350px height=200px  alt=""/></a>
+</div>
+<div class="fy-widget-content"><p>Our chefs headed by Chef Linguini cook with love with the freshest and highest quality ingredients mostly from our farm.</p>
+<p>Come and taste our fantastic cuisine!</p>
+<a class="fy-button fy-button-border fy-button-small" href="https://linguini.forqy.website/menu/">View menu</a>
+
+</div>
+</div>
+
+
+</div>
+
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-4"> 
+<div class="fy-widget-column fy-flex-column-auto fy-flex-column-tablet-50 fy-flex-column-phone-100"><div id="widget_call_to_action-2" class="fy-widget widget_call_to_action"><div class="fy-widget-container fy-widget-has-button fy-widget-has-link"><h3 class="fy-widget-title"><a href="/menu">Napa Valley Wines</a></h3><div class="fy-lazy-parent fy-lazy-parent-loaded"><a href="/menu" class="fy-widget-image fy-lazy fy-lazy-loaded" style="opacity: 1; display: block; background-image: url(&quot;https://linguini.forqy.website/wp-content/uploads/2016/08/linguini-cuisine-1.jpg&quot;);"></a><div class="fy-loading">
+	<div class="fy-loader">
+<a href="#"><img src="images/hinh2.jpg" width="350px height=200px  alt=""/></a>
+</div>
+<div class="fy-widget-content"><p> Famous worldwide for its wine, Napa Valley is a favorite global destination for winery tours and tastings. Here are the very best.</p>
+
+<p>Come and taste our fantastic cuisine!</p>
+<a class="fy-button fy-button-border fy-button-small" href="https://linguini.forqy.website/menu/">View wines</a>
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-md-4"> 
+<div class="fy-widget-column fy-flex-column-auto fy-flex-column-tablet-50 fy-flex-column-phone-100"><div id="widget_call_to_action-2" class="fy-widget widget_call_to_action"><div class="fy-widget-container fy-widget-has-button fy-widget-has-link"><h3 class="fy-widget-title"><a href="/menu">Book a Table Online</a></h3><div class="fy-lazy-parent fy-lazy-parent-loaded"><a href="/menu" class="fy-widget-image fy-lazy fy-lazy-loaded" style="opacity: 1; display: block; background-image: url(&quot;https://linguini.forqy.website/wp-content/uploads/2016/08/linguini-cuisine-1.jpg&quot;);"></a><div class="fy-loading">
+	<div class="fy-loader">
+<a href="#"><img src="images/hinh3.jpg" width="350px height=200px  alt=""/></a>
+</div>
+<div class="fy-widget-content"><p>If you want to pre-book your table, you can use our fantastic online booking form, and we will prepare your table for your visit.</p>
+<p>Come and taste our fantastic cuisine!</p>
+<a class="fy-button fy-button-border fy-button-small" href="https://linguini.forqy.website/reservations/">Book a table</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+</body>
+</html>
